d14588bb10fe2386c110a8653a3ccf174c8d66a0
Arturjssln <arturjesslen@gmail.com>
2020-02-20 14:11:42 +0100

    building done

---
+++ contactForces4/src/build/CMakeFiles/contactForces.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/Users/artur/Documents/GitHub/groundForceReaction/contactForces4/build/libcontactForces.dylib"
+  "/Users/artur/Documents/GitHub/groundForceReaction/contactForces4/build/libcontactForces.pdb"
+  "CMakeFiles/contactForces.dir/ContactForceAnalysis.cpp.o"
+  "CMakeFiles/contactForces.dir/ContactForceAnalysisTarget.cpp.o"
+  "CMakeFiles/contactForces.dir/ContactPointOnPlane.cpp.o"
+  "CMakeFiles/contactForces.dir/osimRegisterTypes.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/contactForces.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
